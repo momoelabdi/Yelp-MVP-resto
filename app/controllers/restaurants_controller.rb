f3a3efddccaae08eccbca0a3212c308a6486ab1f
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2022-03-16 18:46:16 +0100

    Yelp MVP

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,36 @@
+class RestaurantsController < ApplicationController
+  # before_action :restaurant_name, only: [ :name ]
+  # before_action :set_restaurant, only: [:show, :edit, :update, :destroy, :info]
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new()
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+end
