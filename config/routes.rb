2460861da167ed91ce4c1a9bc753967233d8c577
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2022-03-16 20:35:00 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants do
-    resources :reviews, only: [:new, :create ]
-  end
-  resources :reviews, only:[ :index, :show, :new, :create ]
-
-
-  # resources :restaurants do
-  #   resources :reviews, only: [ :new, :create ]
-  # end
 end
